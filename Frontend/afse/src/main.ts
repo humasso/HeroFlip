53753ff6062f32cb445fd23b92b008d28693f8c2
humasso <humas.hussain@studenti.unimi.it>
2025-05-13 16:53:53 +0200

    Problemi con Authservice

--- Frontend/afse/src/main.ts
+++ Frontend/afse/src/main.ts
@@ -1,12 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+/*
 import { provideRouter, Routes } from '@angular/router';
 import { LoginComponent } from './app/auth/login/login.component';
 import { RegisterComponent } from './app/auth/register/register.component';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+*/
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
