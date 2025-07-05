3dd57269533e32e94ac0441958ca2d5f814cd297
Humasso <humas.hussain@studenti.unimi.it>
2025-07-05 18:20:51 +0200

    Prima versione shop pachetti

--- Frontend/afse/src/app/shop/shopdash/shopdash.component.spec.ts
+++ Frontend/afse/src/app/shop/shopdash/shopdash.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ShopdashComponent } from './shopdash.component';
 
 describe('ShopdashComponent', () => {
@@ -8,7 +9,11 @@ describe('ShopdashComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ShopdashComponent]
+      imports: [
+        ShopdashComponent,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
 
--- Frontend/afse/src/app/shop/shopdash/shopdash.component.ts
+++ Frontend/afse/src/app/shop/shopdash/shopdash.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-shopdash',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './shopdash.component.html',
   styleUrl: './shopdash.component.css'
 })
-export class ShopdashComponent {
+export class ShopdashComponent implements OnInit {
+  credits = 0;
+  packOptions = Array.from({ length: 10 }, (_, i) => i + 1);
 
+  private userId = localStorage.getItem('userid')?.split('"')[3];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    if (this.userId) {
+      this.userService.getUser(this.userId).subscribe({
+        next: user => this.credits = user.credits, 
+        error: () => {}
+      });
+    }
+  }
+
+  buyPacks(qty: number) {
+    alert(`Hai acquistato ${qty} pacchetto${qty > 1 ? 'i' : ''}!`);
+  }
 }
