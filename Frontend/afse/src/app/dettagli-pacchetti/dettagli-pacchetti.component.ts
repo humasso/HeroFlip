09a9bb11ace499faab2ba3cb638e6d6ba7c30870
humasso <humas.hussain@studenti.unimi.it>
2025-07-10 14:08:57 +0200

    dettagli pacchetti sbaglaito

--- Frontend/afse/src/app/dettagli-pacchetti/dettagli-pacchetti.component.ts
+++ Frontend/afse/src/app/dettagli-pacchetti/dettagli-pacchetti.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Card } from '../models/card.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 interface PackInfo {
   name: string;
@@ -12,7 +14,7 @@ interface PackInfo {
 
 @Component({
   selector: 'app-dettagli-pacchetti',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './dettagli-pacchetti.component.html',
   styleUrl: './dettagli-pacchetti.component.css'
 })
@@ -21,12 +23,47 @@ export class DettagliPacchettiComponent implements OnInit {
   heroes: Card[] = [];
   searchTerm = '';
   totalHeroes = 0;
+  private readonly PACKS: Record<string, PackInfo> = {
+    'Pacchetto Base': {
+      name: 'Pacchetto Base',
+      image: 'assets/pacchetto.png',
+      description: 'Pacchetto base con carte di tutti gli eroi'
+    },
+    'Pacchetto Marvel': {
+      name: 'Pacchetto Marvel',
+      image: 'assets/pacchetto marvel.png',
+      description: 'Pacchetto dedicato agli eroi Marvel',
+      publisher: 'Marvel Comics'
+    },
+    'Pacchetto DC': {
+      name: 'Pacchetto DC',
+      image: 'assets/pacchetto dc.png',
+      description: 'Pacchetto dedicato agli eroi DC',
+      publisher: 'DC Comics'
+    }
+  };
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
-    
+    const name = this.route.snapshot.paramMap.get('name') || '';
+    this.pack = this.PACKS[name] || { name, image: '', description: '' };
+    this.loadHeroes();
   }
 
-  constructor(private route: ActivatedRoute) {}
+  private loadHeroes() {
+    this.http.get<any[]>('assets/heroes.json').subscribe(data => {
+      const filtered = data.filter(h => !this.pack.publisher || h.publisher === this.pack.publisher);
+      this.heroes = filtered.map(h => ({
+        heroId: h.id,
+        name: h.name,
+        image: h.image,
+        publisher: h.publisher,
+        powerstats: {} as any
+      }));
+      this.totalHeroes = this.heroes.length;
+    });
+  }
 
 
   get filteredHeroes(): Card[] {
